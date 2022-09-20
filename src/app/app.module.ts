a328d9b575fac760395c247ab01c027ab0da6585
Simone-Ramundi <simoneramundi@outlook.com>
2022-09-20 21:19:54 +0100

    Creazione componenti Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import { MenuComponent } from './menu/menu.component';
+import { SectionComponent } from './section/section.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    MenuComponent,
+    SectionComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
